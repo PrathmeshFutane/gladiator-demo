c9ff79f314a03f5a2d44040c466c97269b329a18
Iqra Shaikh <Iqra.Shaikh@lntinfotech.com>
2021-03-29 16:58:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  constructor(){
+    console.log("iqra shaikh");
+    
+  }
   title = 'shopping-demo';
+  
+  
+  
 }
